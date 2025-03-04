471f34a49fe64fb7514009ecb75b012e5a47a5ab
SergioAlvJ <84820143+SergioAlvJ@users.noreply.github.com>
2025-03-04 13:19:23 -0700

    Remove unused files and clean up project structure

--- src/profiles/Alain/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ProfileAlain = () => {
-  return <div>Profile Alain</div>;
-};
-
-export default ProfileAlain;
--- src/profiles/Checo/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ProfileCheco = () => {
-  return <div>Profile Checo</div>;
-};
-
-export default ProfileCheco;
